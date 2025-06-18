c04116fc1b155c6f1bbd0437ad2ddefd61cda8fe
anamputt <atthachai3namputta@gmail.com>
2025-06-18 14:13:32 +0000

    ลบโฟลเดอร์ cell05/ex06 ออกเพื่อเริ่มใหม่

--- cell05/ex06/ upcase_it.py
+++
@@ -1,6 +0,0 @@
-def main():
-    if len(sys.argv) == 2:
-        print(sys.argv[1].upper())
-    else:
-        print("none")
-main()
--- cell05/ex06/upcase_it.py
+++
@@ -1,4 +0,0 @@
-def upcase_it(text):
-    return text.upper()
-
-print(upcase_it("rtfm (Read the f-ing manual)"))
